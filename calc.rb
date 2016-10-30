76e6ab9d30d8db8fc307ef7dc97bb1e2c1c57046
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-10-30 12:47:09 +0200

    Remove all spaces

--- calc.rb
+++ calc.rb
@@ -1,3 +1,24 @@
-def calculate(str)
-  42
+def calculate(str)   # "   2 * ( 2 + 2)"
+  str.gsub!(/\s/,'') #Без пробілів "2*(2+2)"
+
+ 
+
+
+
+  return str
+
+
+
+
+
 end
+
+
+#  "2*3+(4+2) + (1+1)".gsub(/\((\d+)\+(\d+)\)/){$1.to_i + $2.to_i}
+
+puts calculate("   2 * ( 2 + 2)") # 8
+puts calculate(" 4 / (1 + 1) ") # 2
+puts calculate("2 * (2 + 2) - 8") # 0
+puts calculate("1 + (-2)") #-1
+puts calculate("2 * 2") # 4
+puts calculate("2 * 2 + 2") # 6
