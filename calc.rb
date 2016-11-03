78c7d58cd9811212be525526711484954a2d2f3c
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-11-03 09:49:01 +0200

    Still not working RPN

--- calc.rb
+++ calc.rb
@@ -7,24 +7,86 @@ def make_rpn(expression)    # "  ( 2 + 3   )* ( 4 - 1)"
                                       #  ["(", "2", "+", "3", ")", "*", "(", "4", "-", "1", ")"]
   priority = { "(" => 1, "+" => 2, "-" => 2, "*" => 3, "/" => 3}
 
-  arr_of_elements.each |element| do
-    if ("0".."9").include?(element) # крута фіча
+  arr_of_elements.each do |element| 
+    if element =~ /\d+/ #("0".."9").include?(element)  #i =~ /\d+/ 
       rpn_str << element
-    elsif element == '('
+    elsif element == "("
       stack << element
-    elsif stack.empty? || priority[stack.last] < priority[element]
+    elsif element == ")"
+      rpn_str << stack.pop until priority[stack.last] == 1
+
+    elsif stack.empty? || (priority[stack.last] < priority[element])
       stack << element
+    elsif (priority[stack.last] > priority[element]) || (priority[stack.last] = priority[element])
+      rpn_str << stack.pop
+      stack << element
+
+    end
+  end
+  while !stack.empty? do
+     rpn_str << stack.pop
+  end
+
+  return rpn_str
+# end
+
+
     
 
+      
 
 
 
+        
 
-
-
-
-
+  #   else
+  #     case element
+  #     when element == "("
+  #       stack << element
+  #     when stack.empty? || priority[stack.last] < priority[element]
+  #       stack << element
+  #     when priority[stack.last] >= priority[element]
+  #       rpn_str << stack.pop until priority[stack.last] == priority[element]  #true
+  #       rpn_str << element
+  #     when element == ")"
+  #       rpn_str << stack.pop until priority[stack.last] == 1
+  #       stack.pop
+  #     end
+  #   end
+  # end
+  # if !stack.empty?
+  #   rpn_str << stack.reverse
+  # end
+  # return rpn_str
 end
+
+
+      
+      
+      
+      
+
+# Рассматриваем поочередно каждый символ:
+# 1. Если этот символ - число (или переменная), то просто помещаем его в выходную строку. +++
+# 2. Если символ - знак операции (+, -, *, / ), то проверяем приоритет данной операции.  +++
+#   Операции умножения и деления имеют наивысший приоритет (допустим он равен 3). 
+#   Операции сложения и вычитания имеют меньший приоритет (равен 2). 
+#   Наименьший приоритет (равен 1) имеет открывающая скобка.
+# Получив один из этих символов, мы должны проверить стек: 
+# а) Если стек все еще пуст, или находящиеся в нем символы ( а находится в нем 
+# могут только знаки операций и открывающая скобка) 
+# имеют меньший приоритет, чем приоритет текущего символа, то помещаем текущий символ в стек. +++
+# б) Если символ, находящийся на вершине стека имеет приоритет, 
+# больший или равный приоритету текущего символа, то извлекаем символы из стека 
+# в выходную строку до тех пор, пока выполняется это условие; затем переходим к пункту а).
+# 3. Если текущий символ - открывающая скобка, то помещаем ее в стек.
+# 4. Если текущий символ - закрывающая скобка, то извлекаем символы из стека в 
+# выходную строку до тех пор, пока не встретим в стеке открывающую скобку 
+# (т.е. символ с приоритетом, равным 1), которую следует просто уничтожить. 
+# Закрывающая скобка также уничтожается.
+
+
+
 
 # на виході має утворитись 23+41-*
 
@@ -32,7 +94,17 @@ end
 
 
 
-puts make_rpn("  ( 2 + 3   )* ( 4 - 1)")
+puts make_rpn("  ( 2 + 3   ) * ( 4 - 1)")
+
+
+
+    # if ("0".."9").include?(element) # крута фіча
+    #   rpn_str << element
+    # elsif element == '('
+    #   stack << element
+    # elsif stack.empty? || priority[stack.last] < priority[element]
+    #   stack << element
+    
 
 
 
